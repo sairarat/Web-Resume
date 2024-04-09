becda4fb4caa181361479f1246997f74c26a4879
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-04-09 12:45:49 +0800

    Added transitions

---
+++ js/scrollReveal.js
@@ -0,0 +1,76 @@
+ScrollReveal().reveal('.fade', {
+    duration: 1000,
+    opacity: 0,
+    easing: 'ease-in-out',
+    reset: true
+  });
+  
+  ScrollReveal().reveal('.fade-left', {
+    duration: 1000,
+    opacity: 0,
+    easing: 'ease-in-out',
+    origin: 'left',
+    distance: '20px',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
+  ScrollReveal().reveal('.fade-right', {
+    duration: 1000,
+    opacity: 0,
+    easing: 'ease-in-out',
+    origin: 'right',
+    distance: '20px',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
+  ScrollReveal().reveal('.fade-up', {
+    duration: 1000,
+    opacity: 0,
+    easing: 'ease-in-out',
+    origin: 'top',
+    distance: '20px',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
+  ScrollReveal().reveal('.fade-down', {
+    duration: 1000,
+    opacity: 0,
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    distance: '20px',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
+  ScrollReveal().reveal('.slide', {
+    duration: 1000,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
+  ScrollReveal().reveal('.rotate', {
+    duration: 1000,
+    rotate: {
+      x: 0,
+      y: 180,
+      z: 0
+    },
+    easing: 'ease-in-out',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
+  ScrollReveal().reveal('.scale', {
+    duration: 1000,
+    scale: 0.5,
+    easing: 'ease-in-out',
+    reset: true,
+    delay: 200 // Delay of 200 milliseconds
+  });
+  
